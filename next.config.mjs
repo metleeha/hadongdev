82993fb606050d338890f818431839c5f871e7f4
Hadong Lee <hadongme@gmail.com>
2024-03-05 21:57:48 -0500

    ⚡ init project

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  styledComponents: {
+    ssr: true,
+  },
+};
+
+export default nextConfig;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default function Page() {
+  return <Heading>Hello, Next.js!</Heading>
+}
+
+const Heading = styled.h1`
+  font-size: 2rem;
+  front-weight: 700;
+  text-decoration: underline;
+`
